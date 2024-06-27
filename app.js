af10edfd4c4c9cd6d5cc00efd8a7d4b62243d7c3
Olexander B <bolex1967@gmail.com>
2024-06-27 10:45:12 +0300

    Less/10-03. Get, Post, Put, Delete

--- app.js
+++ app.js
@@ -5,12 +5,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Читаємо всіх користувачів
+app.get("/users", (req, res) => {
+  res.send("List of users");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About page!");
+// Читаємо дані окремого користувача
+app.get("/users/:id", (req, res) => {
+  res.send("A specific user");
+});
+
+// Створюємо нового користувача
+app.post("/users", (req, res) => {
+  res.send("Users created successfully");
+});
+
+// Змінюємо дані окремого користувача
+app.put("/users/:id", (req, res) => {
+  res.send("This user was updated successfully");
+});
+
+// Видаляємо окремого користувача
+app.delete("/users/:id", (req, res) => {
+  res.send("User deleted successfully");
 });
 
 app.listen(port, () => {
