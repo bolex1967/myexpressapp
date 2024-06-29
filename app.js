ae24fcc983c3a9a04cae2333873c4bfc52c1d87f
Olexander B <bolex1967@gmail.com>
2024-06-29 23:47:32 +0300

    Add Prisma

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 // Замість ES6 будемо використовувати CommonJS
 
 const express = require("express");
-const sqlite3 = require("sqlite3");
+// const sqlite3 = require("sqlite3");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
 
@@ -10,14 +13,10 @@ const port = 3000;
 app.use(express.json());
 
 // Create an in-memory SQLite database
-const db = new sqlite3.Database(":memory:");
-// Create a database structure
+// const db = new sqlite3.Database(":memory:");
+
+// Create a database structure, використовуючи ORM Prisma
 // Initialize a table
-db.serialize(() => {
-  db.run(
-    "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT)"
-  );
-});
 
 // CRUD API Endpoints
 // Create a new user
@@ -25,93 +24,90 @@ db.serialize(() => {
 // In-memory "database" for demonstration purposes («База даних» у пам’яті для демонстраційних цілей.)
 // let users = {};
 
-// *** GET ********************************************************************
-// GET request for fetching all users (GET запит для отримання всіх користувачів)
-app.get("/users", (req, res) => {
-  // res.send("List of users");
+// GET all users **************************************************************
+app.get("/users", async (req, res) => {
   console.log("=== GET ===");
-  // res.status(200).json({ users });
-  db.all(`SELECT * FROM users`, [], (err, rows) => {
-    if (err) {
-      // message - повідомлення, отримане з бази даних
-      return res.status(400).json({ error: err.message });
-    }
-    res.json(rows);
-  });
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
-// *** GET by ID **************************************************************
-// Get a single user by id
-app.get("/users/:id", (req, res) => {
+// Get a single user by id ****************************************************
+app.get("/users/:id", async (req, res) => {
   console.log(`=== GET (by id) ===`);
   const { id } = req.params;
-  db.get(`SELECT * FROM users WHERE id = ?`, [id], (err, row) => {
-    if (err) {
-      return res.status(400).json({ error: err.message });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
     }
-    console.log(row);
-    res.json(row);
-  });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
-// *** POST *******************************************************************
-// POST request for creating a new user (POST запит на створення нового користувача)
-app.post("/users", (req, res) => {
+// POST request for creating a new user ***************************************
+app.post("/users", async (req, res) => {
   console.log("=== POST ===");
   // Сервер очікує отримання id, name, email, які дивимося в тілі запиту req.body
   const { name, email } = req.body;
-  // console.log(users[id], name, email);
-  // Додаємо до нашого об'єкта дані (використовуємо захист sql-ін'єкцію проти атак)
-  // run(запит; масив значень, які ми вставляємо замість ?; функція, яка запускається з запитом)
-  db.run(
-    `INSERT INTO users (name, email) VALUES (?, ?)`,
-    [name, email],
-    function (err) {
-      if (err) {
-        // message - повідомлення, отримане з бази даних
-        return res.status(400).json({ error: err.message });
-      }
-      // Function Declaration – функция, об'явлена в основному потоці коду (класика).
-      // Function Expression – об'явлення функції в контексті виразу, наприклад присвоювання ().
-      // !!! Для коректної роботи 'this', - функція не повинна бути стрілочною.
-      console.log({ id: this.lastID });
-      res.status(201).json({ id: this.lastID });
-    }
-  );
+
+  try {
+    const user = await prisma.user.create({
+      data: { name, email },
+    });
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
-// *** PUT ********************************************************************
-// Update a user's information
-app.put("/users/:id", (req, res) => {
+// Update a user's information ************************************************
+app.put("/users/:id", async (req, res) => {
   console.log("PUT");
   const { id } = req.params;
   const { name, email } = req.body;
-
-  db.run(
-    `UPDATE users SET name = ?, email = ? WHERE id = ?`,
-    [name, email, id],
-    function (err) {
-      if (err) {
-        return res.status(400).json({ error: err.message });
-      }
-      res.json({ message: `Row updated: ${this.changes}` });
-    }
-  );
+  console.log(req.body);
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: { name, email },
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
-// *** DELETE *****************************************************************
-// Delete a user
-app.delete("/users/:id", (req, res) => {
+// Delete a user **************************************************************
+app.delete("/users/:id", async (req, res) => {
   console.log(`=== DELETE ===`);
+  console.log(req.body.name, req.body.email);
   const { id } = req.params;
-  db.run(`DELETE FROM users WHERE id = ?`, [id], function (err) {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-    res.json({ message: `Row deleted: ${this.changes}` });
-  });
+
+  try {
+    await prisma.user.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    console.log(`User id = ${id} (${req.body.name}) - deleted`);
+    res.json({ message: `User (id: ${id}, name: ${req.body.name}) deleted` });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
+// Listening port *************************************************************
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
